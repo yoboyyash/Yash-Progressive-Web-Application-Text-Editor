b2e0c1a5b7e6647cefc758993eef8bfa531381c9
yoboyyash <yashrajbaviskar@gmail.com>
2024-04-15 05:50:58 -0500

    Webpack + packageJson + db

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,22 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault(); 
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    butInstall.classList.toggle('hidden', false);
+
+    // TODO: Implement a click event handler on the `butInstall` element
+    butInstall.addEventListener('click', async () => {
+    event.prompt();
+
+    butInstall.classList.toggle('hidden', true);
+});
+});
+
+
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log("Done!", event);
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// TODO: Add and configure workbox plugins for a service worker and manifest file. line 22-53
+// TODO: Add CSS loaders and babel to webpack. line 60-62 + babel 
 
 module.exports = () => {
   return {
@@ -17,13 +17,60 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // Webpack plugins + generates html file and bundles 
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+
+      
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        inject: true,
+        fingerprints: false,
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Text editor that runs in the browser',
+        background_color: '#ffffff',
+        theme_color: '#2196f3',
+        start_url: './',
+        publicPath: './',
+        display: 'standalone',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      })
       
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              }
+            }
+          }
       ],
     },
   };
